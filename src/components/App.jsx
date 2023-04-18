1553ffe9c25aeab52f3cf6a11fe50d7316a3b55e
raphaelsanchez0 <sanchezraphael0@gmail.com>
2023-04-17 20:37:07 -0400

    styled header and .guesser

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from '../assets/react.svg'
 import Header from './Header.jsx'
+import Guesser from './Guesser'
 
 import '../style.css'
 
@@ -8,9 +9,9 @@ export default function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div className="app">
       <Header />
-
+      <Guesser/>
     </div>
   )
 }
---
+++ src/components/Guesser.jsx
@@ -0,0 +1,10 @@
+import testFlag from '../assets/testFlag.svg'
+
+export default function Guesser(){
+    return(
+        <div className="guesser">
+            <img src = {testFlag} className="guesser--image"/>
+
+        </div>
+    )
+}
