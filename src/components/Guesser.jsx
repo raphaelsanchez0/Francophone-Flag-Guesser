d705eb7dcb02a86e465caf924e1f94b944a01c01
raphaelsanchez0 <sanchezraphael0@gmail.com>
2023-04-18 10:08:14 -0400

    created a random flag code function an implemented a test button

--- src/components/Guesser.jsx
+++ src/components/Guesser.jsx
@@ -1,19 +1,44 @@
-import testFlag from '../assets/testFlag.svg'
+import countries from '../assets/francophoneCountries.js'
+import { useState } from 'react';
 
 export default function Guesser() {
+    const [flag, setFlag] = useState(getRandomCountryCode())
+
+
+
     function getRandomCountryCode() {
+        return getRandomNumber(countries.length)
+    }
+    function getRandomNumber(max) {
+        return Math.floor(Math.random() * (max + 1));
+    }
+
+    function changeFlag(){
+        setFlag(getRandomCountryCode())
+        console.log(flag, countries[flag].country)
+    }
+
+    function createFlagOptions(trueFlagCode, numberOfOptions){
+        const flagOptions = []
+        for(let i =0; i < numberOfOptions; i++){
+            flagOptions.push
+        }
 
     }
 
 
+    
 
     return (
         <div className="guesser">
-            <img src={testFlag} className="guesser--image" />
+            <img src={`https://flagcdn.com/${countries[flag].id}.svg`} className="guesser--image" />
             <div className='guesser--buttons'>
+                <button className='testBtn' onClick={changeFlag}>Change</button>
 
             </div>
 
         </div>
     )
 }
+
+//
