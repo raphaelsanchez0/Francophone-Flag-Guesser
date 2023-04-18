7eb581a5cc8d46ebe6925f3b309a6586e800d304
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2023-04-17 22:26:37 -0400

    created the francophoneCountries.js object, have not added country codes yet

--- src/components/Guesser.jsx
+++ src/components/Guesser.jsx
@@ -1,9 +1,18 @@
 import testFlag from '../assets/testFlag.svg'
 
-export default function Guesser(){
-    return(
+export default function Guesser() {
+    function getRandomCountryCode() {
+
+    }
+
+
+
+    return (
         <div className="guesser">
-            <img src = {testFlag} className="guesser--image"/>
+            <img src={testFlag} className="guesser--image" />
+            <div className='guesser--buttons'>
+
+            </div>
 
         </div>
     )
